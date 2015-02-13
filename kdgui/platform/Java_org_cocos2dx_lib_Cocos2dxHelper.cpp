84df20bd049697e65d0705744f43dc0fcaa1451c
weolar <weolar@qq.com>
2015-02-13 21:19:33 +0800

    * ndk 方面的帮助文件

---
+++ kdgui/platform/Java_org_cocos2dx_lib_Cocos2dxHelper.cpp
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <jni.h>
+#include <android/log.h>
+#include <android/assets_manager.h>
+
+#include <string>
+//#include "JniHelper.h"
+#include "Java_org_cocos2dx_lib_Cocos2dxHelper.h"
+
+using namespace std;
+
+extern "C" {
+
+void Java_org_cocos2dx_lib_Cocos2dxActivity_nativeSetAssetManager(JNIEnv* env, jclass cls, jobject assetManager)
+{
+	gAssetMgr = AAssetManager_fromJava( env, assetManager );
+}
+
+}
