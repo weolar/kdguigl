e8e8a4a2f34893e18d487ecd56c2522e34d8feef
weolar <weolar@qq.com>
2015-02-27 22:08:55 +0800

    * 修改测试案例

--- build/android/assets/demo1.js
+++ build/android/assets/demo1.js
@@ -1,5 +1,9 @@
 ﻿var json = [
-	["rect", {"x":1, "y":20, "width":100, "height":"127", "fill":0xff610, "opacity":1, "id":"test2"},],
+	["rect", {"x":1, "y":20, "width":100, "height":"127", "fill":0xff610, "opacity":1, "id":"test2", "kd-call":function (l, n) {
+	    n.bind("touchbegin", function (e, d) {
+	        ::print("touchbegin");
+	    }, false, null);
+	}},],
 	["image", {"x":121, "y":20, "width":850, "height":672, "src":"res:///image001.png", "id":"test1"},],
 	["path", {"d":"M20 20 C90 40 130 40 180 20 S250 60 280 20", "fill":"none", "stroke":"#0000ff"}],
 	["text", {"x":1, "y":50, "width":100, "height":"127", "fill":0xff610, "opacity":1, "id":"test4", "font-size":55}, "ha哈ha"],
